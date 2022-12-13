ab2bee60b7b21baee364d4fa981ae89af61e75b2
azrdneperone@gmail.com <azrdneperone@gmail.com>
2022-12-13 17:39:21 +0700

    master: Upload 2

--- commerce.sql
+++ commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Des 2022 pada 19.24
+-- Waktu pembuatan: 13 Des 2022 pada 11.37
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 7.4.28
 
@@ -42,10 +42,9 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_barang`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Keripik Pisang', 'Oleh-oleh Khas Lampung', 'Makanan', 30000, 10, 'keripik.jpg'),
-(2, 'manisan', 'Makanan cemilan manis', 'Makanan', 20000, 15, 'manisan.jpg'),
-(3, 'Dodol', 'Cemilan Dodol manis', 'Makanan', 10000, 8, 'dodol.jpg'),
-(4, 'kue pisang', 'makanan khas lampung', 'Makanan', 40000, 7, 'pisang.jpg');
+(1, 'keripik pisang', 'oleh-oleh lampung', 'Makanan', 30000, 10, 'keripik.jpg'),
+(2, 'Lapis Legit', 'Lapis legit biasa disebut dengan spekuk', 'Makanan', 35000, 6, 'legit.jpg'),
+(3, 'Lempuk Durian', 'lempuk disebut juga dengan istilah dodol', 'Makanan', 60000, 6, 'lempuk.jpg');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +55,16 @@ INSERT INTO `tb_barang` (`id_barang`, `nama_brg`, `keterangan`, `kategori`, `har
 --
 ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
